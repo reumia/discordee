22ae92d20f7ce59f37455345da30a8c3e5f92970
TTS기술담당 조현석 <skt.1902644@sk.com>
2017-07-21 14:43:57 +0900

    브랜드 컬러 초기화

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
+import 'reset-css/_reset.scss';
 import './App.scss';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
     render() {
         return (
             <div className="app">
                 <header className="app-header">
-                    Discordee
+                    <em className="title">Discordee.</em>
+                    <span className="description">Discord Server Sharing Platform.</span>
                 </header>
+                <nav className="app-nav">
+
+                </nav>
                 <div className="app-body">
                     BODY
                 </div>

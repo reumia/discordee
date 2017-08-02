b8c108df03b41da27f3fd7d925f4583eb11076fb
TTS기술담당 조현석 <skt.1902644@sk.com>
2017-08-02 11:14:43 +0900

    Functions : 로그인 > 토큰 생성 로직 Functions 적용

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,34 @@
 const functions = require('firebase-functions');
-const request = require('request');
-const cors = require('cors')({origin: true});
+const fetch = require('node-fetch');
+const btoa = require('btoa');
 
-exports.auth = functions.https.onRequest((req, res) => {
-    cors(req, res, () => {
-        return request.get({
-            url: "https://discordapp.com/api/oauth2/authorize",
-            data: {
-                json: {
-                    client_id: '337506226680102914',
-                }
-            }
-        }).on('response', (res) => {
-            console.log('통신완료');
-            console.log(res.statusCode);
-            console.log(res.headers);
-        })
+const API_ROOT = 'https://us-central1-discordee-dd480.cloudfunctions.net';
+const SERVICE_ROOT = 'https://discordee-dd480.firebaseapp.com';
+const CLIENT_ID = '337506226680102914';
+const CLIENT_SECRET = 'ShQRUwC17sE97RUOZVpDwXWrlxmYgwoe';
+const redirect = encodeURIComponent(`${API_ROOT}/callback`);
+
+exports.login = functions.https.onRequest((req, res) => {
+    res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify+guilds&response_type=code&redirect_uri=${redirect}&permissions=32`);
+});
+
+exports.callback = functions.https.onRequest((req, res) => {
+    if (!req.query.code) throw new Error('NoCodeProvided');
+    const code = req.query.code;
+    const credits = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
+    const fetchUrl = `https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}`;
+    console.log('fetching...', fetchUrl);
+
+    fetch(fetchUrl, {
+        method: 'POST',
+        headers: {
+            Authorization: `Basic ${credits}`,
+        },
+    }).then((res) => {
+        return res.json();
+    }).then((json) => {
+        console.log('callback response...', json);
+        res.cookie('discordee_api_token', json.access_token);
+        res.redirect(SERVICE_ROOT);
     });
 });
